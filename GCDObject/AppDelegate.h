7f026e2d92726f6140264f8209d579f6e3ff3b38
wei qi <weiqi@hzdracom.com>
2016-08-02 19:38:37 +0800

    Initial Commit

---
+++ GCDObject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCDObject
+//
+//  Created by 魏琦 on 16/8/2.
+//  Copyright © 2016年 com.drcacom.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCDObject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCDObject
+//
+//  Created by 魏琦 on 16/8/2.
+//  Copyright © 2016年 com.drcacom.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
